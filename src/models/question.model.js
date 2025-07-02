da1513cf9df9bc6cb5427280e4e4a079465e1f4a
mridu <mridudixit15@gmail.com>
2025-07-02 20:21:31 +0530

    updt

--- src/models/question.model.js
+++ src/models/question.model.js
@@ -7,7 +7,19 @@ const optionSchema = new mongoose.Schema({
 
 const questionSchema = new mongoose.Schema({
   text: { type: Map, of: String, required: true },
-  options: { type: [optionSchema], validate: v => v.length === 4 }
+  options: { 
+    type: [optionSchema], 
+    validate: [
+      {
+        validator: v => v.length === 4,
+        message: 'Exactly 4 options are required.'
+      },
+      {
+        validator: v => v.filter(opt => opt.isCorrect).length === 1,
+        message: 'Exactly one option must be marked as correct.'
+      }
+    ]
+  }
 });
 
 module.exports = mongoose.model('Question', questionSchema);
